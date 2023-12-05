2a010f7abd041121b04c01a08d729fc8322ca395
Ashish Kumar Behera <ashish.behera@aniccadata.in>
2023-12-05 21:17:11 +0530

    Final Layout changes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -72,16 +72,16 @@ export default function LandingPage() {
                                 </div>
                                 <div className="self-stretch max-md:max-w-full max-md:pr-5">
                                     <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                                        <div className="flex flex-col items-stretch max-md:w-full max-md:ml-0">
+                                        <div className="flex flex-col  max-md:w-full max-md:ml-0">
                                             <Image
                                                     loading="lazy"
                                                     src={feature}
-                                                    className="-ml-20 min-w-[500px]"
+                                                    className="min-w-[500px] -ml-20"
                                                     alt="new"
                                             />
                                         </div>
-                                        <div className="flex flex-col items-stretch w-[57%] ml-5 max-md:w-full max-md:ml-0">
-                                            <div className="flex flex-col mt-4 items-end max-md:max-w-full max-md:mt-10">
+                                        <div className="flex flex-col items-stretch w-[57%] ml-5 max-md:w-full max-md:ml-0 mt-6">
+                                            <div className="flex flex-col mt-10 items-end max-md:max-w-full max-md:mt-10">
                                                 <div className="flex w-[249px] max-w-full flex-col items-end">
                                                         <Image
                                                             loading="lazy"
