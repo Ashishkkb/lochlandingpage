63b8548d90a1b0478b0940c018d228366c132892
Ashish Kumar Behera <ashish.behera@aniccadata.in>
2023-12-05 21:53:25 +0530

    new changes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -49,7 +49,7 @@ export default function LandingPage() {
                             <div className="flex flex-col px-16 items-end max-md:max-w-full max-md:px-5">
                                 <div className="self-stretch max-md:max-w-full max-md:pr-5">
                                     <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                                        <div className="flex flex-col items-stretch w-[51%] max-md:w-full max-md:ml-0">
+                                        <div className="flex flex-col items-stretch w-[51%] max-md:w-full max-md:ml-0 justify-between">
                                             <div className="flex grow flex-col items-start mt-2 max-md:mt-3.5">
                                                 <Image
                                                     loading="lazy"
@@ -67,7 +67,9 @@ export default function LandingPage() {
                                                 </div>
                                             </div>
                                         </div>
+                                        <div className="w-[400px] ml-10">
                                         <TopBar />
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="self-stretch max-md:max-w-full max-md:pr-5">
